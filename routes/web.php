04115146ff3b24967ee4025c0d6bec5199dc5d39
malith94batugedara <malithbatugedara@gmail.com>
2023-08-20 14:50:26 +0530

    categoryaddedandfetch

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +24,13 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->middleware('auth','isAdmin')->group(function(){
+
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+    Route::get('/category', [CategoryController::class, 'index'])->name('admin.category');
+
+    Route::get('/add-category', [CategoryController::class, 'create'])->name('admin.add-category');
+
+    Route::post('/add-category', [CategoryController::class, 'store'])->name('admin.add-category');
+    
 });
